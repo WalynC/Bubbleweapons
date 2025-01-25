38ae27891a2e7eb474c108b80c55975c52a12f9e
Walyn <walyncurtis@gmail.com>
2025-01-24 20:15:46 -0600

    Begins work on player tools

---
+++ Assets/Scripts/Player/Tool.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public abstract class Tool : MonoBehaviour
+{
+    public abstract void Use();
+}
---
+++ Assets/Scripts/Player/Tools/Gun.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Gun : Tool
+{
+    public override void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+}
