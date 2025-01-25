5beb486168cba38afd383e67c69e39fb65f93d04
Walyn <walyncurtis@gmail.com>
2025-01-24 20:04:08 -0600

    Current project progress

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    InputAction lookAction;
+    float vertRot = 0;
+    float horiRot = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        lookAction = InputSystem.actions.FindAction("Look");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 look = lookAction.ReadValue<Vector2>();
+        horiRot += look.x;
+        vertRot -= look.y;
+        vertRot = Mathf.Clamp(vertRot, -90f, 90f);
+        transform.rotation = Quaternion.Euler(vertRot, horiRot,0);
+    }
+}
---
+++ Assets/Scripts/FPSController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FPSController : MonoBehaviour
+{
+    CharacterController characterController;
+    public Camera cam;
+    public float speed;
+    public float jumpHeight;
+    public float timeToApex = .5f;
+    float jumpSpeed;
+    float gravity;
+
+    Vector3 velocity = Vector3.zero;
+    InputAction moveAction;
+    InputAction jumpAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+
+        jumpSpeed = 2 * jumpHeight / timeToApex;
+        gravity = -2 * jumpHeight / Mathf.Pow(timeToApex, 2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 inputDir = moveAction.ReadValue<Vector2>();
+        Vector3 right = cam.transform.TransformDirection(Vector3.right);
+        Vector3 forward = Vector3.Cross(right, Vector3.up);
+        velocity = new Vector3(0, velocity.y, 0) + (right * inputDir.x * speed) + (forward * inputDir.y * speed);
+        if (!characterController.isGrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        else if (jumpAction.ReadValue<float>() > .5f)
+        {
+            velocity.y = jumpSpeed;
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
