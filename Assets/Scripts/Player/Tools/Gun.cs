7a2d81458883745c5be8970617aa34aa93a76a81
Walyn <walyncurtis@gmail.com>
2025-01-24 20:43:39 -0600

    Gun now shoots bullets

--- Assets/Scripts/Player/Tools/Gun.cs
+++ Assets/Scripts/Player/Tools/Gun.cs
@@ -1,9 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : Tool
 {
+    public GameObject bullet;
+    public Queue<GameObject> pool = new Queue<GameObject>();
+
     public override void Use()
     {
-        print("BANG BANG BANG");
+        GameObject obj = GetFromPool();
+        obj.transform.position = transform.position;
+    }
+
+    GameObject GetFromPool()
+    {
+        if (pool.Count == 0)
+        {
+            pool.Enqueue(Instantiate(bullet));
+        }
+        return pool.Dequeue();
     }
 }
