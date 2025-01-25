9eddcababa388c15746b460bf5304fc44ab3b5f7
Walyn <walyncurtis@gmail.com>
2025-01-25 08:08:57 -0600

    Enemy now wanders around

--- Assets/Scripts/Enemy/BasicEnemy.cs
+++ Assets/Scripts/Enemy/BasicEnemy.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BasicEnemy : MonoBehaviour
 {
+    public float wanderRange = 5f;
+    Vector3 home;
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        home = transform.position;
+    }
+
     public void Die()
     {
         gameObject.SetActive(false);
+    }
+
+    public void FixedUpdate()
+    {
+        if (agent.remainingDistance < 0.1f)
+        {
+            agent.SetDestination(home+new Vector3(Random.Range(-wanderRange,wanderRange), 0, Random.Range(-wanderRange, wanderRange)));
+        }
     }
 }
